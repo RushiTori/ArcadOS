d63aaa61b0cd452b55f2ae0def3078b2e31a4c3a
RushiTori <Arcturusarc09@gmail.com>
2025-11-01 14:24:12 +0100

    completely revamped the display module to now depend entirely on the future shape_algo module

--- src/engine/display.asm
+++ src/engine/display.asm
@@ -1,520 +1,390 @@
-bits 64
+bits             64
 
 %include "engine/display.inc"
 
-section .data
+section          .data
 
-current_color:
-static current_color:data
-	db 0x1F
+var(static, uint8_t, current_display_color, 0x1F)
+var(static, pointer_t, display_buffer_addr, NULL)
+var(static, uint8_t, draw_line_ex_radius, 0)
 
-section .text
+section          .text
 
-; args : u8 color
-set_color:
-global set_color:function
-	mov byte[current_color], dil
+; pointer_t get_display_buffer(void);
+function(global, get_display_buffer)
+	mov rax, pointer_p [display_buffer_addr]
 	ret
 
-; args : 
-clear_screen:
-global clear_screen:function
-	mov al, byte[current_color]
+; void set_display_buffer(pointer_t buffer);
+function(global, set_display_buffer)
+	mov pointer_p [display_buffer_addr], rdi
+	ret
 
-	mov rcx, (SCREEN_WIDTH * SCREEN_HEIGHT)
-	mov rdi, VGA_MEMORY_ADDR
+; uint8_t get_display_color(void);
+function(global, get_display_color)
+	mov al, uint8_p [current_display_color]
+	ret
+
+; void set_display_color(uint8_t col);
+function(global, set_display_color)
+	mov uint8_p [current_display_color], dil
+	ret
+
+; void put_pixel(uint16_t x, uint16_t y);
+function(global, put_pixel)
+	and rdi, 0xFFFF
+	cmp rdi, DISPLAY_WIDTH
+	jae .end               ; if (x >= DISPLAY_WIDTH) return;
+
+	and rsi, 0xFFFF
+	cmp rsi, DISPLAY_HEIGHT
+	jae .end                ; if (y >= DISPLAY_HEIGHT) return;
+
+	mov rax, DISPLAY_WIDTH
+	mul rsi                ; rax = y * DISPLAY_WIDTH
+	add rdi, rax           ; rdi = y * DISPLAY_WIDTH + x
+
+	push rdi
+	call get_display_buffer
+	pop  rdi
+
+	add rdi, rax ; rdi = display_buffer + y * DISPLAY_WIDTH + x
+
+	push rdi
+	call get_display_color
+	pop  rdi
+	
+	mov uint8_p [rdi], al
+
+	.end:
+	ret
+
+; void put_pixel_vec(ScreenVec2 vec);
+function(global, put_pixel_vec)
+	call screenvec2_unpack
+	mov  di, ax
+	mov  si, dx
+	jmp  put_pixel
+
+; void clear_screen(void);
+function(global, clear_screen)
+	call get_color
+	mov  dil, al
+	jmp  clear_screen_c
+
+; void clear_screen_c(uint8_t col);
+function(global, clear_screen_c)
+	mov al,  dil
+	mov rcx, DISPLAY_BUFFER_SIZE
 	rep stosb
 	ret
 
-; args : u16 x, u16 y
-put_pixel:
-global put_pixel:function
-	cmp di, SCREEN_WIDTH
-	jae .end
-	cmp si, SCREEN_HEIGHT
-	jae .end
-
-	mov rax, SCREEN_WIDTH
-	imul ax, si
-	add ax, di
-
-	push rbx
-	mov bl, byte[current_color]
-	mov byte[VGA_MEMORY_ADDR + rax], bl
-	pop rbx
-	.end:
-	ret
-
-; args : u16 x, u16 y, u16 len
-put_pixel_row:
-global put_pixel_row:function
-	.draw_loop:
-		cmp dx, 0
-		je .end
-
-		call put_pixel
-
-		inc di
-		dec dx
-
-		jmp .draw_loop
-	.end:
-	ret
-
-; args : u16 x, u16 y, u16 size
-draw_square:
-global draw_square:function
+; void draw_square(uint16_t x, uint16_t y, uint16_t size);
+function(global, draw_square)
 	mov cx, dx
-	call draw_rect
-	ret
+	jmp draw_rect
 
-; args : u16 x, u16 y, u16 w, u16 h
-draw_rect:
-global draw_rect:function
-	mov r10w, di
-	mov r11w, dx
-	.draw_loop:
-		cmp cx, 0
-		je .end
+; void draw_square_vec(ScreenVec2 pos, uint16_t size);
+function(global, draw_square_vec)
+	mov dx, si
+	jmp draw_rect_vec
 
-		call put_pixel_row
-		mov di, r10w
-		mov dx, r11w
-		inc si
-		dec cx
-
-		jmp .draw_loop
-	.end:
-	ret
-
-; args : u16 x, u16 y, u16 size
-draw_square_line:
-global draw_square_line:function
+; void draw_square_line(uint16_t x, uint16_t y, uint16_t size);
+function(global, draw_square_line)
 	mov cx, dx
-	call draw_rect_line
+	jmp draw_rect_line
+
+; void draw_square_line_vec(ScreenVec2 pos, uint16_t size);
+function(global, draw_square_line_vec)
+	mov dx, si
+	jmp draw_rect_line_vec
+
+; void draw_square_line_ex(uint16_t x, uint16_t y, uint16_t size, uint8_t thickness);
+function(global, draw_square_line_ex)
+	mov r8b, cl
+	mov cx,  dx
+	jmp draw_rect_line_ex
+
+; void draw_square_line_ex_vec(ScreenVec2 pos, uint16_t size, uint8_t thickness);
+function(global, draw_square_line_ex_vec)
+	mov cl, dl
+	mov dx, si
+	jmp draw_rect_line_ex_vec
+
+; void draw_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+function(global, draw_rect)
+	lea r8, [put_pixel]
+	jmp rect_fill_algo
+
+; void draw_rect_vec(ScreenVec2 pos, ScreeVec2 sizes);
+function(global, draw_rect_vec)
+	lea rdx, [put_pixel_vec]
+	jmp rect_fill_algo_vec
+
+; void draw_rect_line(uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+function(global, draw_rect_line)
+	lea r8, [put_pixel]
+	jmp rect_line_algo
+
+; void draw_rect_line_vec(ScreenVec2 pos, ScreeVec2 sizes);
+function(global, draw_rect_line_vec)
+	lea rdx, [put_pixel_vec]
+	jmp rect_line_algo_vec
+
+; void draw_rect_line_ex(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t thickness);
+function(global, draw_rect_line_ex)
+	cmp r8b, 0
+	je  .nothing_to_draw
+	cmp r8b, 1
+	je  draw_rect_line
+
+	push rbp      ; setting up the stack frame
+	mov  rbp, rsp
+
+	push r12 ; preserve r12
+	push r13 ; preserve r13
+	push r14 ; preserve r14
+	push r15 ; preserve r15
+	push rbx ; preserve rbx
+
+	; uint8_t thick = thickness;
+	; uint16_t thick_2 = thickness * 2;
+	;
+	; Top
+	; draw_rect(x - thick, y - thick, w + thick_2, thick_2);
+	;
+	; Bottom
+	; draw_rect(x - thick, y + h, w + thick_2, thick_2);
+	;
+	; Left
+	; draw_rect(x - thick, y, thick_2, h);
+	;
+	; Right
+	; draw_rect(x + w, y, thick_2, h);
+
+	and rdi, 0xFFFF
+	and rsi, 0xFFFF
+	and rdx, 0xFFFF
+	and rcx, 0xFFFF
+	and r8,  0xFF
+
+	sub di,   r8w ; x - thick
+	mov r12w, di  ; r12 = x - thick
+
+	mov r13w, si  ; r13 = y
+	sub si,   r8w ; y - thick
+
+	shl r8w, 1   ; thick_2
+	mov bx,  r8w ; rbx = thick_2
+
+	add dx,   r8w ; w + thick_2
+	mov r14w, dx  ; r14 = w + thick_2
+
+	mov r15w, cx ; r15 = h
+
+	mov  cx, r8w   ; thick_2
+	call draw_rect ; draw_rect(x - thick, y - thick, w + thick_2, thick_2);
+
+	mov  di, r12w  ; x - thick
+	mov  si, r13w  ; y
+	add  si, r15w  ; y + h
+	mov  dx, r14w  ; w + thick_2
+	mov  cx, bx    ; thick_2
+	call draw_rect ; draw_rect(x - thick, y + h, w + thick_2, thick_2);
+
+	mov  di, r12w  ; x - thick
+	mov  si, r13w  ; y
+	mov  dx, bx    ; thick_2
+	mov  cx, r15w  ; h
+	call draw_rect ; draw_rect(x - thick, y, thick_2, h);
+
+	mov  si,   r13w ; y
+	mov  dx,   bx   ; thick_2
+	mov  cx,   r15w ; h
+	sub  r14w, bx   ; w
+	shr  bx,   1    ; thick
+	mov  di,   r12w ; x - thick
+	sub  di,   bx   ; x
+	add  di,   r14w ; x + w
+	call draw_rect  ; draw_rect(x + w, y, thick_2, h);
+
+	pop rbx ; restore rbx
+	pop r15 ; restore r15
+	pop r14 ; restore r14
+	pop r13 ; restore r13
+	pop r12 ; restore r12
+
+	pop rbp ; exiting the stack frame
+	.nothing_to_draw:
 	ret
 
-; args : u16 x, u16 y, u16 w, u16 h
-draw_rect_line:
-global draw_rect_line:function
-	mov r8w, 1
-	call draw_rect_line_ex
+; void draw_rect_line_ex_vec(ScreenVec2 pos, ScreeVec2 sizes, uint8_t thickness);
+function(global, draw_rect_line_ex_vec)
+	cmp dl, 0
+	je  .nothing_to_draw
+	cmp dl, 1
+	je  draw_rect_line_vec
+
+	push rbp      ; setting up the stack frame
+	mov  rbp, rsp
+
+	mov r8b, dl ; r8b = thickness
+
+	push rsi               ; push sizes
+	push r8                ; push thickness
+	call screenvec2_unpack ; screenvec2_unpack(pos);
+	pop  r8                ; r8b = thickness
+	pop  rdi               ; edi = sizes
+
+	push r8  ; push thickness
+	push rax ; push pos.x
+	push rdx ; push pos.y
+
+	sub  rsp, 8            ; to re-align the stack
+	call screenvec2_unpack ; screenvec2_unpack(sizes);
+	add  rsp, 8
+
+	mov cx, dx ; cx = sizes.y
+	mov dx, ax ; dx = sizes.x
+	pop rsi    ; si = pos.y
+	pop rdi    ; di = pos.x
+	pop r8     ; r8b = thickness
+
+	pop rbp               ; exiting the stack frame
+	jmp draw_rect_line_ex
+
+	.nothing_to_draw:
 	ret
 
-; args : u16 x, u16 y, u16 size, u16 thickness
-draw_square_line_ex:
-global draw_square_line_ex:function
-	mov r8w, cx
-	mov cx, dx
-	call draw_rect_line_ex
-	ret
+; void draw_triangle(ScreenVec2 a, ScreenVec2 b, ScreenVec2 c);
+function(global, draw_triangle)
+	lea rcx, [put_pixel_vec]
+	jmp triangle_fill_algo_vec
 
-; args : u16 x, u16 y, u16 w, u16 h, u16 thickness
-draw_rect_line_ex:
-global draw_rect_line_ex:function
-	cmp r8w, 0
-	je .end
+; void draw_triangle_line(ScreenVec2 a, ScreenVec2 b, ScreenVec2 c);
+function(global, draw_triangle_line)
+	lea rcx, [put_pixel_vec]
+	jmp triangle_line_algo_vec
+
+; void draw_triangle_line_ex(ScreenVec2 a, ScreenVec2 b, ScreenVec2 c, uint8_t thickness);
+function(global, draw_triangle_line_ex)
+	cmp cl, 0
+	je  .nothing_to_draw
+	cmp cl, 1
+	je  draw_triangle_line
+
+	push rbp      ; setting up the stack frame
+	mov  rbp, rsp
+
+	push r12 ; preserve r12
+	push r13 ; preserve r13
+	push r14 ; preserve r14
+	push r15 ; preserve r15
+
+	mov r12, rdi ; r12 = a
+	mov r13, rsi ; r13 = b
+	mov r14, rdx ; r14 = c
+	mov r15, rcx ; r15 = thickness
 	
-	; if  the thickness is greater than half the width or height
-	; then we can simply draw a rectangle
-	mov r11w, dx
-	shr r11w, 1
-	cmp r8w, r11w
-	jge .simplify_call
-	
-	mov r11w, cx
-	shr r11w, 1
-	cmp r8w, r11w
-	jge .simplify_call
+	mov  rdx, r15     ; rdx = thickness
+	call draw_line_ex ; draw_line_ex(a, b, thickness);
 
-	push r12
-	push r13
-	push r14
-	push r15
+	mov  rdi, r13     ; rdi = b
+	mov  rsi, r14     ; rsi = c
+	mov  rdx, r15     ; rdx = thickness
+	call draw_line_ex ; draw_line_ex(b, c, thickness);
 
-	mov r12w, di ; x
-	mov r14w, dx ; w
-	mov r15w, cx ; h
+	mov  rdi, r12     ; rdi = a
+	mov  rsi, r14     ; rsi = c
+	mov  rdx, r15     ; rdx = thickness
+	call draw_line_ex ; draw_line_ex(b, c, thickness);
 
-	mov cx, r8w ; thickness
-	; draw_rect(x, y, w, thickness)
-	call draw_rect
+	pop r15 ; restore r15
+	pop r14 ; restore r14
+	pop r13 ; restore r13
+	pop r12 ; restore r12
 
-	; here si (y) is now y + thickness
-	mov r13w, si ; y + thickness
-
-	shl cx, 1 ; thickness * 2
-	mov r9w, cx ; backup thickness * 2 in r9w
-	mov cx, r15w ; cx = h
-	sub cx, r9w ; cx -= thickness * 2
-	; here cx (h) is now h - (thickness * 2)
-
-	mov dx, r8w ; thickness
-	; draw_rect(x, y + thickness, thickness, h - (thickness * 2))
-	call draw_rect
-
-	; here si (y) is now y + h - thickness
-	mov r11w, si
-	mov si, r13w
-
-	mov cx, r15w ; cx = h
-	sub cx, r9w ; cx -= thickness * 2
-	; here cx (h) is back to h - (thickness * 2)
-
-	add di, r14w ; x + w
-	sub di, r8w ; (x + w) - thickness
-	; draw_rect(x + w - thickness, y + thickness, thickness, h - (thickness * 2))
-	call draw_rect
-	; here si (y) is (y + thickness) + (h - (thickness * 2)) ==> y + h - thickness
-
-	mov di, r12w ; x
-	mov dx, r14w ; w
-	mov cx, r8w ; thickness
-	; draw_rect(x, y + h - thickness, w, thickness)
-	call draw_rect
-
-	pop r15
-	pop r14
-	pop r13
-	pop r12
-
-	jmp .end
-
-	.simplify_call:
-		call draw_rect
-
-	.end:
+	pop rbp ; exiting the stack frame
+	.nothing_to_draw:
 	ret
 
-; Bresenham's Line algorithm
-; Sources : 
-;   - Bresenham's Line Algorithm - Demystified Step by Step : https://www.youtube.com/watch?v=CceepU1vIKo 
-; args : u16 x0, u16 y0, u16 x1, u16 y1
-draw_line:
-global draw_line:function
-	mov r8w, dx
-	sub r8w, di
-	cmp di, dx
-	jle .skip_abs_x
-	imul r8w, -1
-	.skip_abs_x:
+; void draw_circle(uint16_t x, uint16_t y, uint8_t r);
+function(global, draw_circle)
+	lea rcx, [put_pixel]
+	jmp circle_fill_algo
 
-	mov r9w, cx
-	sub r9w, si
-	cmp si, cx
-	jle .skip_abs_y
-	imul r9w, -1
-	.skip_abs_y:
+; void draw_circle_vec(ScreenVec2 pos, uint8_t r);
+function(global, draw_circle_vec)
+	lea rdx, [put_pixel_vec]
+	jmp circle_fill_algo_vec
+
+; void draw_circle_line(uint16_t x, uint16_t y, uint8_t r);
+function(global, draw_circle_line)
+	lea rcx, [put_pixel]
+	jmp circle_algo
+
+; void draw_circle_line_vec(ScreenVec2 pos, uint8_t r);
+function(global, draw_circle_line_vec)
+	lea rdx, [put_pixel_vec]
+	jmp circle_algo_vec
+
+; void draw_line(uint16_t aX, uint16_t aY, uint16_t bX, uint16_t bY);
+function(global, draw_line)
+	lea r8, [put_pixel]
+	jmp line_algo
+
+; void draw_line_vec(ScreenVec2 a, ScreenVec2 b);
+function(global, draw_line_vec)
+	lea rdx, [put_pixel_vec]
+	jmp line_algo_vec
+
+; void draw_line_ex_cb(uint16_t x, uint16_t y);
+function(static, draw_line_ex_cb)
+	mov dl, uint8_p [draw_line_ex_radius]
+	jmp draw_circle
+
+; void draw_line_ex(uint16_t aX, uint16_t aY, uint16_t bX, uint16_t bY, uint8_t thickness);
+function(global, draw_line_ex)
+	cmp r8b, 0
+	je  .nothing_to_draw
+	cmp r8b, 1
+	je  draw_line
+
+	mov uint8_p [draw_line_ex_radius], r8b
 	
-	cmp r8w, r9w
-	jge draw_lineH
-	jl draw_lineV
+	lea  r8, [draw_line_ex_cb]
+	call line_algo
 
-	; ret : the ret is handled by both draw_lineH and draw_lineV
-
-; args : u16 x0, u16 y0, u16 x1, u16 y1
-draw_lineH:
-static draw_lineH:function
-	cmp di, dx
-	je .end ; if x0 == x1 there is no line to draw
-	jl .skip_swap_end_points ; if the line already points right, no need to swap the end points
-	xchg di, dx ; swaps x0 and x1
-	xchg si, cx ; swaps y0 and y1
-	.skip_swap_end_points:
-	
-	mov r8w, dx
-	sub r8w, di ; delta_x = x1 - x0
-
-	mov r9w, cx
-	sub r9w, si ; delta_y = y1 - y0
-
-	mov r10w, 1 ; dir = 1
-	cmp si, cx
-	jle .skip_correct_dir ; if the line already points down, no need to correct the dir or delta_y
-	mov r10w, -1
-	imul r9w, r10w
-	.skip_correct_dir:
-
-	shl r9w, 1 ; delta_y *= 2
-
-	mov r11w, r9w
-	sub r11w, r8w ; p = delta_y * 2 - delta_x
-	
-	shl r8w, 1 ; delta_x *= 2
-
-	.draw_loop:
-		call put_pixel
-
-		cmp r11w, 0
-		jl .skip_update_y
-		add si, r10w ; y += dir
-		sub r11w, r8w ; p -= delta_x * 2
-		.skip_update_y:
-		add r11w, r9w ; p += delta_y * 2
-		
-		inc di
-		cmp di, dx
-		jne .draw_loop
-
-	.end:
+	.nothing_to_draw:
 	ret
 
-; args : u16 x0, u16 y0, u16 x1, u16 y1
-draw_lineV:
-static draw_lineV:function
-	cmp si, cx
-	je .end
-	jl .skip_swap_end_points
-	xchg si, cx
-	xchg di, dx
-	.skip_swap_end_points:
+; void draw_line_ex_vec(ScreenVec2 a, ScreenVec2 b, uint8_t thickness);
+function(global, draw_line_ex_vec)
+	push rbp      ; setting up the stack frame
+	mov  rbp, rsp
 
-	mov r8w, dx
-	sub r8w, di
+	mov r8b, dl ; r8b = thickness
 
-	mov r9w, cx
-	sub r9w, si
+	push rsi               ; push b
+	push r8                ; push thickness
+	call screenvec2_unpack ; screenvec2_unpack(a);
+	pop  r8                ; r8b = thickness
+	pop  rdi               ; edi = b
 
-	mov r10w, 1
-	cmp di, dx
-	jle .skip_correct_dir
-	mov r10w, -1
-	imul r8w, r10w
-	.skip_correct_dir:
+	push r8  ; push thickness
+	push rax ; push a.x
+	push rdx ; push a.y
 
-	shl r8w, 1
+	sub  rsp, 8            ; to re-align the stack
+	call screenvec2_unpack ; screenvec2_unpack(b);
+	add  rsp, 8
 
-	mov r11w, r8w
-	sub r11w, r9w
-	
-	shl r9w, 1
+	mov cx, dx ; cx = b.y
+	mov dx, ax ; dx = b.x
+	pop rsi    ; si = a.y
+	pop rdi    ; di = a.x
+	pop r8     ; r8b = thickness
 
-	.draw_loop:
-		call put_pixel
+	pop rbp ; exiting the stack frame
 
-		cmp r11w, 0
-		jl .skip_update_x
-		add di, r10w
-		sub r11w, r9w
-		.skip_update_x:
-		add r11w, r8w
-		
-		inc si
-		cmp si, cx
-		jne .draw_loop
-
-	.end:
-	ret
-
-; Bresenham's Midpoint algorithm
-; Sources : 
-;   - The Midpoint Circle Algorithm Explained Step by Step : https://www.youtube.com/watch?v=hpiILbMkF9w
-; args : u16 x, u16 y, u16 radius
-draw_circle:
-global draw_circle:function
-	; WIP
-	cmp dx, 0
-	je .fast_end
-
-	mov r8w, di ; cx
-	mov r9w, si ; cy
-	mov r10w, dx
-	neg r10w ; p = -r
-
-	xor di, di ; x = 0
-	mov si, r10w ; y = -r
-	.draw_loop:
-		cmp r10w, 0
-		jle .skip_inc_y
-
-		inc si ; y++
-		add r10w, si ; p += y*2
-		add r10w, si
-		.skip_inc_y:
-
-		add r10w, di ; p += x*2
-		add r10w, di
-
-		inc r10w ; p++
-
-		; Now we can put 4 rows of pixels
-
-		; putPixelRow(cx - x, cy + y, (cx + x) - (cx - x))
-		; putPixelRow(cx - x, cy + y, x + x)
-		mov dx, di
-		shl dx, 1
-		neg di
-		add di, r8w
-		add si, r9w
-		call put_pixel_row
-		sub di, r8w
-		sub si, r9w
-
-		; putPixelRow(cx - x, cy - y, (cx + x) - (cx - x))
-		; putPixelRow(cx - x, cy - y, x + x)
-		mov dx, di
-		shl dx, 1
-		neg di
-		add di, r8w
-		neg si
-		add si, r9w
-		call put_pixel_row
-		sub di, r8w
-		sub si, r9w
-		neg si
-
-		neg si
-		xchg di, si
-
-		; putPixelRow(cx - y, cy + x, (cx + y) - (cx - y))
-		; putPixelRow(cx - y, cy + x, y + y)
-		mov dx, di
-		shl dx, 1
-		neg di
-		add di, r8w
-		add si, r9w
-		call put_pixel_row
-		sub di, r8w
-		sub si, r9w
-
-		; putPixelRow(cx - y, cy - x, (cx + y) - (cx - y))
-		; putPixelRow(cx - y, cy - x, y + y)
-		mov dx, di
-		shl dx, 1
-		neg di
-		add di, r8w
-		neg si
-		add si, r9w
-		call put_pixel_row
-		sub di, r8w
-		sub si, r9w
-		neg si
-
-		xchg di, si
-		neg si
-
-		neg si
-		cmp di, si
-		jge .end ; if x >= -y : return
-
-		neg si
-		inc di ; x++
-		jmp .draw_loop
-
-	.end:
-	mov di, r8w ; recover cx
-	mov si, r9w ; recover cy
-	.fast_end:
-	ret
-
-; Bresenham's Midpoint algorithm
-; Sources : 
-;   - The Midpoint Circle Algorithm Explained Step by Step : https://www.youtube.com/watch?v=hpiILbMkF9w
-; args : u16 x, u16 y, u16 radius
-draw_circle_line:
-global draw_circle_line:function
-	cmp dx, 0
-	je .fast_end
-
-	mov r8w, di ; cx
-	mov r9w, si ; cy
-	mov r10w, dx
-	neg r10w ; p = -r
-
-	xor di, di ; x = 0
-	mov si, r10w ; y = -r
-	.draw_loop:
-		cmp r10w, 0
-		jle .skip_inc_y
-
-		inc si ; y++
-		add r10w, si ; p += y*2
-		add r10w, si
-		.skip_inc_y:
-
-		add r10w, di ; p += x*2
-		add r10w, di
-
-		inc r10w ; p++
-
-		; Now we can put 8 pixels
-
-		add di, r8w ; x + cx
-		add si, r9w ; y + cy
-
-		; putPixel(cx + x, cy + y)
-		call put_pixel
-
-		sub di, r8w ; x
-		neg di ; -x
-		add di, r8w ; -x + cx
-
-		; putPixel(cx - x, cy + y)
-		call put_pixel
-
-		sub si, r9w ; y
-		neg si ; -y
-		add si, r9w ; -y + cy
-
-		; putPixel(cx - x, cy - y)
-		call put_pixel
-
-		sub di, r8w ; -x
-		neg di ; x
-		add di, r8w ; x + cx
-
-		; putPixel(cx + x, cy - y)
-		call put_pixel
-
-		sub di, r8w ; x
-		sub si, r9w ; -y
-		neg si ; y
-
-		xchg di, si ; swap x and y
-
-		add di, r8w ; y + cx
-		add si, r9w ; x + cy
-
-		; putPixel(cx + y, cy + x)
-		call put_pixel
-
-		sub di, r8w ; y
-		neg di ; -y
-		add di, r8w ; -y + cx
-
-		; putPixel(cx - y, cy + x)
-		call put_pixel
-
-		sub si, r9w ; x
-		neg si ; -x
-		add si, r9w ; -x + cy
-
-		; putPixel(cx - y, cy - x)
-		call put_pixel
-
-		sub di, r8w ; -y
-		neg di ; y
-		add di, r8w ; y + cx
-
-		; putPixel(cx + y, cy - x)
-		call put_pixel
-
-		sub di, r8w ; y
-		sub si, r9w ; -x
-		neg si ; x
-
-		xchg di, si ; swap x and y again
-
-		neg si
-		cmp di, si
-		jge .end ; if x >= -y : return
-
-		neg si
-		inc di ; x++
-		jmp .draw_loop
-
-	.end:
-	mov di, r8w ; recover cx
-	mov si, r9w ; recover cy
-	.fast_end:
-	ret
+	jmp draw_line_ex
