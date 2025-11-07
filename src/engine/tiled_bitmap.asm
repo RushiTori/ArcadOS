ad2c7dbe74ae5e969bce872d646f7ad8945737e4
RushiTori <lucie.fontaine@latoile.em-lyon.com>
2025-11-07 07:47:17 +0100

    implemented the draw functions for tiled_bitmap module

--- src/engine/tiled_bitmap.asm
+++ src/engine/tiled_bitmap.asm
@@ -1,6 +1,21 @@
-bits         64
+bits    64
 
 %include "engine/tiled_bitmap.inc"
+
+section .rodata
+
+tile_as_bitmap:
+static  tile_as_bitmap: data
+	istruc Bitmap
+        at .bits,   dq tile_data
+        at .width,  dw 8
+        at .height, dw 8
+        at .padding db 0, 0, 0, 0
+	iend
+
+section      .bss
+
+res(static,  uint64_t, tile_data)
 
 section      .text
 
@@ -70,20 +85,62 @@ func(global, tiled_bitmap_set_tile_indexed)
 
 ; void draw_tiled_bitmap(const TiledBitmap* tile_map, uint16_t tileX, uint16_t tileY, uint16_t posX, uint16_t posY);
 func(global, draw_tiled_bitmap)
-    ; WIP
-    ret
+    push rcx    ; preserve posY
+    push rdx    ; preserve posX
+    sub  rsp, 8 ; to re-align the stack
+
+    call tiled_bitmap_get_tile ; tiled_bitmap_get_tile(tile_map, tileX, tileY);
+
+    mov uint64_p [tile_data], rax ; tile_data = tiled_bitmap_get_tile(tile_map, tileX, tileY);
+
+    lea rdi, [tile_as_bitmap] ; rdi = &tile_as_bitmap
+
+    add rsp, 8      ; to re-align the stack
+    pop rsi         ; restore posX
+    pop rdx         ; restore posY
+    jmp draw_bitmap ; draw_bitmap(&tile_as_bitmap, posX, posY);
 
 ; void draw_tiled_bitmap_vec(const TiledBitmap* tile_map, ScreenVec2 tilePos, ScreenVec2 screenPos);
 func(global, draw_tiled_bitmap_vec)
-    ; WIP
-    ret
+    push rdx ; preserve screenPos
+
+    call tiled_bitmap_get_tile_vec ; tiled_bitmap_get_tile_vec(tile_map, tilePos);
+
+    pop rsi ; restore screenPos
+
+    mov uint64_p [tile_data], rax ; tile_data = tiled_bitmap_get_tile_vec(tile_map, tilePos);
+
+    lea rdi, [tile_as_bitmap] ; rdi = &tile_as_bitmap;
+
+    jmp draw_bitmap_vec ; draw_bitmap_vec(&tile_as_bitmap, screenPos);
 
 ; void draw_tiled_bitmap_indexed(const TiledBitmap* tile_map, uint32_t idx, uint16_t posX, uint16_t posY);
 func(global, draw_tiled_bitmap_indexed)
-    ; WIP
-    ret
+    push rcx    ; preserve posY
+    push rdx    ; preserve posX
+    sub  rsp, 8 ; to re-align the stack
+
+    call tiled_bitmap_get_tile_indexed ; tiled_bitmap_get_tile_indexed(tile_map, idx);
+
+    mov uint64_p [tile_data], rax ; tile_data = tiled_bitmap_get_tile_indexed(tile_map, idx);
+
+    lea rdi, [tile_as_bitmap] ; rdi = &tile_as_bitmap
+
+    add rsp, 8      ; to re-align the stack
+    pop rsi         ; restore posX
+    pop rdx         ; restore posY
+    jmp draw_bitmap ; draw_bitmap(&tile_as_bitmap, posX, posY);
 
 ; void draw_tiled_bitmap_indexed_vec(const TiledBitmap* tile_map, uint32_t idx, ScreenVec2 screenPos);
 func(global, draw_tiled_bitmap_indexed_vec)
-    ; WIP
-    ret
+    push rdx ; preserve screenPos
+
+    call tiled_bitmap_get_tile_indexed_vec ; tiled_bitmap_get_tile_indexed_vec(tile_map, idx);
+
+    pop rsi ; restore screenPos
+
+    mov uint64_p [tile_data], rax ; tile_data = tiled_bitmap_get_tile_indexed(tile_map, idx);
+
+    lea rdi, [tile_as_bitmap] ; rdi = &tile_as_bitmap;
+
+    jmp draw_bitmap_vec ; draw_bitmap_vec(&tile_as_bitmap, screenPos);
