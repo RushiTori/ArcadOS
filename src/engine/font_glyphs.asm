7a1f2ec5ce07e167b252866ee92ea3a7590fecfa
RushiTori <Arcturusarc09@gmail.com>
2025-11-09 15:08:58 +0100

    finished implementing draw_glyph_background functions

--- src/engine/font_glyphs.asm
+++ src/engine/font_glyphs.asm
@@ -36,7 +36,7 @@ section .text
 	jmp %1                       ; %1(&current_font_data, glyph, x, y);
 %endmacro
 
-%macro __base_body_and_jump_to 1
+%macro __simple_glyph_base_body_and_jump_to 1
 	push rdi ; preserve glyph
 	push rsi ; preserve x
 	push rdx ; preserve y
@@ -50,7 +50,7 @@ section .text
 	jmp %1     ; %1(glyph, x, y, get_font_color());
 %endmacro
 
-%macro __base_vec_body_and_jump_to 1
+%macro __simple_glyph_base_vec_body_and_jump_to 1
 	push rdi ; preserve glyph
 
 	mov  rdi, rsi
@@ -62,7 +62,7 @@ section .text
 	jmp %1     ; %1(glyph, pos.x, pos.y);
 %endmacro
 
-%macro __base_c_vec_body_and_jump_to 1
+%macro __simple_glyph_base_c_vec_body_and_jump_to 1
 	push rdi    ; preserve glyph
 	push rdx    ; preserve col
 	sub  rsp, 8 ; to re-align the stack
@@ -78,6 +78,68 @@ section .text
 	jmp %1      ; %1(glyph, pos.x, pos.y, col);
 %endmacro
 
+%macro __binary_glyph_base_body_and_jump_to__ 3
+	push rdi ; preserve glyph
+	push rsi ; preserve x
+	push rdx ; preserve y
+
+	call %2 ; %2();
+
+	push rax    ; preserve %2();
+	sub  rsp, 8 ; to re-align the stack
+
+	call %3 ; %3();
+
+	mov r8b, al ; r8b = %3();
+	add rsp, 8  ; to re-align the stack
+	pop rdx     ; restore %2();
+	pop rdx     ; restore y
+	pop rsi     ; restore x
+	pop rdi     ; restore glyph
+	jmp %1      ; %1(glyph, x, y, %2(), %3());
+%endmacro
+
+%define __binary_glyph_base_body_and_jump_to(jump, mainCol, secondCol) __binary_glyph_base_body_and_jump_to__ jump, mainCol, secondCol
+
+%macro __binary_glyph_base_c_body__ 2
+	sub rsp, 8 ; to align the stack and make room to preserve the args
+
+	mov uint8_p [rsp],             dil ; preserve glyph
+	mov uint16_p [rsp + 1],        si  ; preserve x
+	mov uint16_p [rsp + 1 + 2],    dx  ; preserve y
+	mov uint8_p [rsp + 1 + 2 + 2], cl  ; preserve frontCol
+
+	mov  cl, r8b ; cl = backCol
+	call %2      ; %2(glyph, x, y, backCol);
+
+	mov dil, uint8_p [rsp]             ; restore glyph
+	mov si,  uint16_p [rsp + 1]        ; restore x
+	mov dx,  uint16_p [rsp + 1 + 2]    ; restore y
+	mov cl,  uint8_p [rsp + 1 + 2 + 2] ; restore frontCol
+
+	call %1 ; %1(glyph, x, y, frontCol);
+
+	add rsp, 8 ; to exit the stack frame
+%endmacro
+
+%define __binary_glyph_base_c_body(frontCall, backCall) __binary_glyph_base_c_body__ frontCall, backCall
+
+%macro __binary_glyph_base_c_vec_body_and_jump_to 1
+	push rdi ; preserve glyph
+	push rdx ; preserve fontCol
+	push rcx ; preserve backCol
+
+	mov  rdi, rsi
+	call screenvec2_unpack ; screenvec2_unpack(pos);
+
+	pop r8     ; restore backCol
+	pop rcx    ; restore fontCol
+	; mov dx, dx                      ; pos.y
+	mov si, ax ; pos.x
+	pop rdi    ; restore glyph
+	jmp %1     ; %1(glyph, pos.x, pos.y, fontCol, backCol);
+%endmacro
+
 ; void draw_glyph_base(uint8_t glyph, uint16_t x, uint16_t y, uint8_t col);
 func(static, draw_glyph_base)
 	mov uint8_p [current_font_data.main_color], cl ; current_font_data.main_color = col
@@ -86,11 +148,11 @@ func(static, draw_glyph_base)
 
 ; void draw_glyph(uint8_t glyph, uint16_t x, uint16_t y);
 func(global, draw_glyph)
-	__base_body_and_jump_to draw_glyph_base
+	__simple_glyph_base_body_and_jump_to draw_glyph_base
 
 ; void draw_glyph_vec(uint8_t glyph, ScreenVec2 pos);
 func(global, draw_glyph_vec)
-	__base_vec_body_and_jump_to draw_glyph
+	__simple_glyph_base_vec_body_and_jump_to draw_glyph
 
 ; void draw_glyph_c(uint8_t glyph, uint16_t x, uint16_t y, uint8_t col);
 func(global, draw_glyph_c)
@@ -98,7 +160,7 @@ func(global, draw_glyph_c)
 
 ; void draw_glyph_c_vec(uint8_t glyph, ScreenVec2 pos, uint8_t col);
 func(global, draw_glyph_c_vec)
-	__base_c_vec_body_and_jump_to draw_glyph_base
+	__simple_glyph_base_c_vec_body_and_jump_to draw_glyph_base
 
 ; void draw_glyph_background_base(uint8_t glyph, uint16_t x, uint16_t y, uint8_t col);
 func(static, draw_glyph_background_base)
@@ -108,11 +170,11 @@ func(static, draw_glyph_background_base)
 
 ; void draw_glyph_background(uint8_t glyph, uint16_t x, uint16_t y);
 func(global, draw_glyph_background)
-	__base_body_and_jump_to draw_glyph_background_base
+	__simple_glyph_base_body_and_jump_to draw_glyph_background_base
 
 ; void draw_glyph_background_vec(uint8_t glyph, ScreenVec2 pos);
 func(global, draw_glyph_background_vec)
-	__base_vec_body_and_jump_to draw_glyph_background
+	__simple_glyph_base_vec_body_and_jump_to draw_glyph_background
 
 ; void draw_glyph_background_c(uint8_t glyph, uint16_t x, uint16_t y, uint8_t col);
 func(global, draw_glyph_background_c)
@@ -120,7 +182,7 @@ func(global, draw_glyph_background_c)
 
 ; void draw_glyph_background_c_vec(uint8_t glyph, ScreenVec2 pos, uint8_t col);
 func(global, draw_glyph_background_c_vec)
-	__base_c_vec_body_and_jump_to draw_glyph_background_base
+	__simple_glyph_base_c_vec_body_and_jump_to draw_glyph_background_base
 
 ; void draw_glyph_shadow_base(uint8_t glyph, uint16_t x, uint16_t y, uint8_t col);
 func(static, draw_glyph_shadow_base)
@@ -133,11 +195,11 @@ func(static, draw_glyph_shadow_base)
 
 ; void draw_glyph_shadow(uint8_t glyph, uint16_t x, uint16_t y);
 func(global, draw_glyph_shadow)
-	__base_body_and_jump_to draw_glyph_shadow_base
+	__simple_glyph_base_body_and_jump_to draw_glyph_shadow_base
 
 ; void draw_glyph_shadow_vec(uint8_t glyph, ScreenVec2 pos);
 func(global, draw_glyph_shadow_vec)
-	__base_vec_body_and_jump_to draw_glyph_shadow
+	__simple_glyph_base_vec_body_and_jump_to draw_glyph_shadow
 
 ; void draw_glyph_shadow_c(uint8_t glyph, uint16_t x, uint16_t y, uint8_t col);
 func(global, draw_glyph_shadow_c)
@@ -145,50 +207,21 @@ func(global, draw_glyph_shadow_c)
 
 ; void draw_glyph_shadow_c_vec(uint8_t glyph, ScreenVec2 pos, uint8_t col);
 func(global, draw_glyph_shadow_c_vec)
-	__base_c_vec_body_and_jump_to draw_glyph_shadow_base
+	__simple_glyph_base_c_vec_body_and_jump_to draw_glyph_shadow_base
 
 ; void draw_glyph_and_background(uint8_t glyph, uint16_t x, uint16_t y);
 func(global, draw_glyph_and_background)
-	push rdi ; preserve glyph
-	push rsi ; preserve x
-	push rdx ; preserve y
-
-	call get_font_color ; get_font_color();
-
-	push rax    ; preserve get_font_color();
-	sub  rsp, 8 ; to re-align the stack
-
-	call get_font_background_color ; get_font_background_color();
-
-	mov r8b, al                     ; r8b = get_font_background_color();
-	add rsp, 8                      ; to re-align the stack
-	pop rdx                         ; restore get_font_color();
-	pop rdx                         ; restore y
-	pop rsi                         ; restore x
-	pop rdi                         ; restore glyph
-	jmp draw_glyph_and_background_c ; draw_glyph_and_background_c(glyph, x, y, get_font_color(), get_font_background_color());
+	__binary_glyph_base_body_and_jump_to(draw_glyph_and_background_c, get_font_color, get_font_background_color)
 
 ; void draw_glyph_and_background_vec(uint8_t glyph, ScreenVec2 pos);
 func(global, draw_glyph_and_background_vec)
-	__base_vec_body_and_jump_to draw_glyph_shadow
+	__simple_glyph_base_vec_body_and_jump_to draw_glyph_and_background
 
 ; void draw_glyph_and_background_c(uint8_t glyph, uint16_t x, uint16_t y, uint8_t fontCol, uint8_t backCol);
 func(global, draw_glyph_and_background_c)
-	; WIP
+	__binary_glyph_base_c_body(draw_glyph_c, draw_glyph_background_c)
 	ret
 
 ; void draw_glyph_and_background_c_vec(uint8_t glyph, ScreenVec2 pos, uint8_t fontCol, uint8_t backCol);
 func(global, draw_glyph_and_background_c_vec)
-	push rdi ; preserve glyph
-	push rdx ; preserve fontCol
-	push rcx ; preserve backCol
-
-	mov  rdi, rsi
-	call screenvec2_unpack ; screenvec2_unpack(pos);
-
-	pop r8                          ; restore backCol
-	pop rcx                         ; restore fontCol
-	; mov dx, dx                      ; pos.y
-	mov si, ax                      ; pos.x
-	pop rdi                         ; restore glyph
-	jmp draw_glyph_and_background_c ; draw_glyph_and_background_c(glyph, pos.x, pos.y, fontCol, backCol);
+	__binary_glyph_base_c_vec_body_and_jump_to draw_glyph_and_background_c
